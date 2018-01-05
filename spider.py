70f879653e7b3fcc236fd5f85edae63ccd86c2b6
kuz0 <kuz0@outlook.com>
2018-01-05 20:37:02 +0800

    new config.py, update spider.py

--- spider.py
+++ spider.py
@@ -1,11 +1,19 @@
-# -*- coding: utf-8 -*-
-import json
+import os
 import re
-from urllib.parse import urlencode
+import json
+import pymongo
 import requests
-from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError
+from urllib.parse import urlencode
 from pyquery import PyQuery
-from requests import RequestException
+from hashlib import md5
+from json import JSONDecodeError
+from multiprocessing import Pool
+from config import *
+
+
+client = pymongo.MongoClient(MONGO_URL, connect=False)
+db = client[MONGO_DB]
 
 
 def get_page_index(offset, keyword):
@@ -18,22 +26,27 @@ def get_page_index(offset, keyword):
         'cur_tab': '3',
         'from': 'gallery',
     }
-    url = 'https://www.toutiao.com/search_content/?' + urlencode(data)
+    params = urlencode(data)
+    base = 'https://www.toutiao.com/search_content/'
+    url = base + '?' + params
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.text
         return None
-    except RequestException:
-        print('请求索引页出错')
+    except ConnectionError:
+        print('Error occurred')
         return None
 
 
-def parse_page_index(html):
-    data = json.loads(html)
-    if data and 'data' in data.keys():
-        for item in data.get('data'):
-            yield item.get('article_url')
+def parse_page_index(text):
+    try:
+        data = json.loads(text)
+        if data and 'data' in data.keys():
+            for item in data.get('data'):
+                yield item.get('article_url')
+    except JSONDecodeError:
+        pass
 
 
 def get_page_detail(url):
@@ -42,28 +55,76 @@ def get_page_detail(url):
         if response.status_code == 200:
             return response.text
         return None
-    except RequestException:
-        print('请求详情页出错', url)
+    except ConnectionError:
+        print('Error occurred')
         return None
 
 
-def parse_page_detail(html):
+def parse_page_detail(html, url):
     doc = PyQuery(html)
     title = doc('title').text()
-    print(title)
-    images_pattern = re.compile('gallery: JSON.parse\((.*?)\)', re.S)
+    images_pattern = re.compile('gallery: JSON.parse\("(.*?)"\)', re.S)
     result = re.search(images_pattern, html)
     if result:
-        print(result.group(0))
+        result_ = result.group(1).replace(r'\"', r'"')
+        result__ = result_.replace(r'\\', r'')
+        data = json.loads(result__)
+        if data and 'sub_images' in data.keys():
+            sub_images = data.get('sub_images')
+            images = [item.get('url') for item in sub_images]
+            for image in images:
+                download_image(image)
+            return {
+                'title': title,
+                'url': url,
+                'images': images,
+            }
 
 
-def main():
-    html = get_page_index(0, '街拍')
-    for url in parse_page_index(html):
+def save_to_mongodb(result):
+    if db[MONGO_TABLE].insert(result):
+        print('Successfully Saved to Mongo', result)
+        return True
+    else:
+        return False
+
+
+def download_image(url):
+    print('Downloading', url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_image(response.content)
+        return None
+    except ConnectionError:
+        return None
+
+
+def save_image(content):
+    file_path = '{0}/{1}.{2}'.format(os.getcwd(),
+                                     md5(content).hexdigest(),
+                                     'jpg')
+    print(file_path)
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+            f.close()
+
+
+def main(offset):
+    text = get_page_index(offset, KEYWORD)
+    urls = parse_page_index(text)
+    for url in urls:
         html = get_page_detail(url)
-        if html:
-            parse_page_detail(html)
+        result = parse_page_detail(html, url)
+        if result:
+            save_to_mongodb(result)
 
 
 if __name__ == '__main__':
-    main()
+    pool = Pool()
+    groups = [x * 20 for x in range(GROUP_START, GROUP_END + 1)]
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
+
